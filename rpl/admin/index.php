8f57125bba0c174082493940f48e5c369f33626b
Chronos-png <ychronos@gmail.com>
2024-05-28 00:11:12 +0700

    sedang mencoba membuat halaman admin

--- rpl/admin/index.php
+++ rpl/admin/index.php
@@ -3,22 +3,17 @@ require_once('../Database/base.php');
 require_once('../Database/database.php');
 ?>
 
-<style>
-    form div input {
-        margin-bottom: 2vh;
-    }
-</style>
 
-<?php
-include("../templates/navbar.php")
-?>
-
-<div class="container" style="padding-top: 10vh; padding-bottom: 10vh;">
+<!-- <div class="container" style="padding-top: 10vh; padding-bottom: 10vh;">
     <div class="list-group" style="background-color: transparent;">
         <a href="listallproduk.php" class="list-group-item list-group-item-action list-group-item-dark">Management Produk</a>
         <a href="tambahproduk.php" class="list-group-item list-group-item-action list-group-item-dark">Tambah Produk</a>
 
     </div>
+</div> -->
+<div class="d-flex" style="overflow: hidden;">
+    <div class="m-0" style="background-color: #242323; height:80vh; width:20vw;"></div>
+    <div class="m-0" style="height:80vh; width:60vw;"></div>
 </div>
 
 
