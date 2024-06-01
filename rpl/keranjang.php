c7cd4aa73b67b22234dbf2ca792698d932732446
Chronos-png <ychronos@gmail.com>
2024-06-01 19:13:54 +0700

    kocak aing

--- rpl/keranjang.php
+++ rpl/keranjang.php
@@ -348,28 +348,17 @@ include("templates/navbar.php")
             <div class="title-info">Metode Pembayaran</div>
             <div class="payment-info" style="display: flex; flex-direction:column; color:white; font-family:'Inter'; font-size:12px; padding: 1.5vh 0 1.5vh 0;">
                 <div class="form-check">
-                    <input class="form-check-input" style="width:15px; height:15px; margin:3px 15px 0 0;" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                    <input class="form-check-input" style="width:15px; height:15px; margin:3px 15px 0 0;" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="Bank Jatim" checked>
                     <label class="form-check-label" for="flexRadioDefault1" style="margin: 3px 0 0 0;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
-                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
-                            <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
-                        </svg>
-                        <span style="font-size: 12px; font-family:'Inter'; padding-left:10px; font-style:italic;"> Credit Card</span>
+                        <span style="font-size: 12px; font-family:'Inter'; padding-left:10px; font-style:italic;"> Transfer Bank Jatim</span>
                     </label>
                 </div>
 
 
                 <div class="form-check">
-                    <input class="form-check-input" style="width:15px; height:15px; margin:3px 15px 0 0;" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <input class="form-check-input" style="width:15px; height:15px; margin:3px 15px 0 0;" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="BRI">
                     <label class="form-check-label" for="flexRadioDefault1" style="margin: 3px 0 0 0;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-qr-code" viewBox="0 0 16 16">
-                            <path d="M2 2h2v2H2z" />
-                            <path d="M6 0v6H0V0zM5 1H1v4h4zM4 12H2v2h2z" />
-                            <path d="M6 10v6H0v-6zm-5 1v4h4v-4zm11-9h2v2h-2z" />
-                            <path d="M10 0v6h6V0zm5 1v4h-4V1zM8 1V0h1v2H8v2H7V1zm0 5V4h1v2zM6 8V7h1V6h1v2h1V7h5v1h-4v1H7V8zm0 0v1H2V8H1v1H0V7h3v1zm10 1h-1V7h1zm-1 0h-1v2h2v-1h-1zm-4 0h2v1h-1v1h-1zm2 3v-1h-1v1h-1v1H9v1h3v-2zm0 0h3v1h-2v1h-1zm-4-1v1h1v-2H7v1z" />
-                            <path d="M7 12h1v3h4v1H7zm9 2v2h-3v-1h2v-1z" />
-                        </svg>
-                        <span style="font-size: 12px; font-family:'Inter'; padding-left:10px; padding-top:4px; font-style:italic;"> QRIS</span>
+                        <span style="font-size: 12px; font-family:'Inter'; padding-left:10px; padding-top:4px; font-style:italic;"> Transfer BRI</span>
                     </label>
                 </div>
 
@@ -380,7 +369,7 @@ include("templates/navbar.php")
                 <input type="hidden" name="harga_ongkir" id="harga_ongkir" value="<?= $cost; ?>">
                 <input type="hidden" name="jumlah_keseluruhan_harga" id="jumlah_keseluruhan_harga" value="<?= $sumprice + $cost; ?>">
                 <!-- /HIDDEN INPUT -->
-                <div class="payment-input">
+                <div class="payment-input" id="cardpayment">
                     <div class="payment-input-txt">
                         <label for="nameoncard">Name On Card</label>
                         <input type="text" name="nameoncard" id="nameoncard" placeholder="Nama,ex : Babang Santoso">
